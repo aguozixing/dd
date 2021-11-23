18cf48be3ad4dbb05f688ec423adbc2a47e81c52
aguozixing <92850260+aguozixing@users.noreply.github.com>
2021-11-23 20:37:16 +0800

    aaa
    aaa

---
+++ Entry/src/main/java/com/yr/entry/Account.java
@@ -0,0 +1,56 @@
+package com.yr.entry;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+
+	private int id;
+	private String name;
+	private String password;
+	private int age;
+	private String head;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getHead() {
+		return head;
+	}
+	public void setHead(String head) {
+		this.head = head;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", password=" + password + ", age=" + age + ", head=" + head
+				+ ", email=" + email + "]";
+	}
+
+
+}
